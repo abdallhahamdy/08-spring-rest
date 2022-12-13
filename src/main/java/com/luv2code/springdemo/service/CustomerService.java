19a2af772cc878eff0e572e80814f017fb8783e1
Abdallha Hamdy <43465167+abdallhahamdy@users.noreply.github.com>
2022-12-13 23:17:39 +0200

    crm-web-app-rest-client-demo

--- src/main/java/com/luv2code/springdemo/service/CustomerService.java
+++ src/main/java/com/luv2code/springdemo/service/CustomerService.java
@@ -2,7 +2,7 @@ package com.luv2code.springdemo.service;
 
 import java.util.List;
 
-import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.model.Customer;
 
 public interface CustomerService {
 
